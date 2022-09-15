eecf285ec2cf3b63e57de368e25bf4907e6e8b0a
sonapingo <sonapingo@hotmail.com>
2022-09-15 14:41:05 +0800

    second commit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def get_request(url, attr):
     global ret
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'}
-    trytimes = 3
+    trytimes = 10
     for i in range(trytimes):
         try:
             ret = requests.get(url, headers=headers, timeout=5)
@@ -37,38 +37,47 @@ def get_cities(html):
     for provinces in html:
         for province in provinces:
             province_name = province.a.get_text()
-            datas[province_name]={}
+            datas[province_name] = {}
             province_url = base_url + province.a.get('href')
             print(province_name)
             cities = get_request(province_url, 'citytr')
-            city_dict={}
+            city_dict = {}
             for city in cities:
                 city_name = city.find_all('a')[1].get_text()
-                city_dict[city_name]={}
                 city_url = base_url + city.find_all('a')[1].get('href')
                 print(city_name)
                 counties = get_request(city_url, 'countytr')
-                county_dict={}
-                for county in counties:
-                    if len(county.find_all('a')) == 0:
-                        continue
-                    else:
-                        county_name = county.find_all('a')[1].get_text()
-                        county_dict[county_name]={}
-                        city_after_url = city.find_all('a')[1].get('href')
-                        city_url_list = city_after_url.split('/')
-                        city_num = city_url_list[0]
-                        county_url = base_url + city_num + '/' + county.find_all('a')[1].get('href')
-                        print(county_name)
-                        towns = get_request(county_url, 'towntr')
-                        town_list = []
-                        for town in towns:
-                            town_name = town.find_all('a')[1].get_text()
-                            print(town_name)
-                            town_list.append(town_name)
-                        county_dict[county_name]=town_list
-                        city_dict[city_name].update(county_dict)
-                        datas[province_name].update(city_dict)
+                if len(counties) == 0:
+                    towns = get_request(city_url, 'towntr')
+                    town_list = []
+                    for town in towns:
+                        town_name = town.find_all('a')[1].get_text()
+                        print(town_name)
+                        town_list.append(town_name)
+                    city_dict[city_name] = town_list
+                    datas[province_name].update(city_dict)
+                else:
+                    city_dict[city_name] = {}
+                    county_dict = {}
+                    for county in counties:
+                        if len(county.find_all('a')) == 0:
+                            continue
+                        else:
+                            county_name = county.find_all('a')[1].get_text()
+                            city_after_url = city.find_all('a')[1].get('href')
+                            city_url_list = city_after_url.split('/')
+                            city_num = city_url_list[0]
+                            county_url = base_url + city_num + '/' + county.find_all('a')[1].get('href')
+                            print(county_name)
+                            towns = get_request(county_url, 'towntr')
+                            town_list = []
+                            for town in towns:
+                                town_name = town.find_all('a')[1].get_text()
+                                print(town_name)
+                                town_list.append(town_name)
+                            county_dict[county_name] = town_list
+                            city_dict[city_name].update(county_dict)
+                            datas[province_name].update(city_dict)
     return datas
 
 
